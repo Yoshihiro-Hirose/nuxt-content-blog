93946df2686e52d22e2150e32306a1d9aa62f28d
Yoshihiro-Hirose <Yoshihiro_Hirose@sis2.saison.co.jp>
2020-06-11 23:26:50 +0900

    初回コミット

---
+++ pages/articles/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h2>{{ article.title }}</h2>
+    <nuxt-content :document="article" />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  async asyncData({ $content, params }) {
+    const article = await $content(`articles/${params.slug}`).fetch()
+
+    return {
+      article
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(article, index) in articles" :key="index">
+      <nuxt-link :to="article.path">{{ article.title }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  async asyncData({ $content }) {
+    const articles = await $content('articles')
+      .limit(5)
+      .fetch()
+
+    return {
+      articles
+    }
+  }
+}
+</script>
